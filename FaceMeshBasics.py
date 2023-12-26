e17a410d224552474f4ee3d723e25a027b684a17
Bohdan Vodianyk <bohdanvodianyk@gmail.com>
2023-12-26 22:44:13 +0100

    simple face mesh

---
+++ FaceMeshBasics.py
@@ -0,0 +1,32 @@
+import mediapipe as mp
+import cv2
+import time
+
+cap = cv2.VideoCapture("videos/4.mp4")
+pTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=3)
+drawSpec = mpDraw.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=2)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_TESSELATION,
+                                  drawSpec, drawSpec)
+            for id, lm in enumerate(faceLms.landmark):
+                ih, iw, ic = img.shape
+                x, y = int(lm.x * iw), int(lm.y * ih)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f"FPS: {int(fps)}", (20, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (0, 255, 0), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
